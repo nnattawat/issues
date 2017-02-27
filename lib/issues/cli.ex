c8c1c723c3b3bc8af895821ee064f4a123bbdfa2
nnattawat <armmer1@gmail.com>
2017-02-27 21:07:06 +1100

    Add CLI to main file

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -5,7 +5,7 @@ defmodule Issues.CLI do
   that end up generating a table of the last _n_ issues in a github project
   """
 
-  def run(argv) do
+  def main(argv) do
     argv
     |> parse_args
     |> process
@@ -43,6 +43,7 @@ defmodule Issues.CLI do
     |> sort_into_ascending_order
     |> Enum.take(count)
     |> format
+    |> IO.inspect
   end
 
   def decode_response({:ok, body}), do: body
